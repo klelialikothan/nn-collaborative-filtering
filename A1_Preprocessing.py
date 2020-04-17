bbdf88420b8b3e0e4fe3e647662ee7dd64addf4f
klelialikothan <up1054352@upnet.gr>
2020-04-17 03:43:44 +0300

    A1 and A2 functional and commented. Results need to be checked CAREFULLY...

--- A1_Preprocessing.py
+++ A1_Preprocessing.py
@@ -1,33 +1,50 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split, KFold
 
+# Basic variables
 N: int = 943
 M: int = 1682
 file = 'u.data'
 col_names = ['user id', 'movie id', 'rating', 'timestamp']
+
+# Import file to dataframe
 df = pd.read_table(file, names=col_names, usecols=col_names[0:3], dtype=np.int32)
-df = df.sort_values(by=['user id', 'movie id'])
-ratings_list = []
-for i in range(0, N):
-    temp = np.zeros(M)
+user_ratings = np.zeros([N, M])
+
+# Process ratings and save to file
+for i in range(0, N):   # foreach user in dataset
+    # foreach rating of a unique user, centre and regularise data
     u = df.loc[df['user id'] == (i+1)]
-    rated = np.array([k for j, k in zip(u['movie id'], u['rating'])])
-    rated = rated.reshape(-1, 1)
-    rated = StandardScaler().fit_transform(X=rated)
+    temp = np.array([k for j, k in zip(u['movie id'], u['rating'])])
+    temp = temp.reshape(-1, 1)
+    temp = StandardScaler().fit_transform(X=temp)
+    # store (existing) ratings in array row, filling empty cells with 0
     x = 0
     for j, k in zip(u['movie id'], u['rating']):
-        temp[j-1] = rated[x]
+        user_ratings[i, (j - 1)] = temp[x]
         x += 1
-    ratings_list.append(temp)
+np.save('user_ratings.npy', user_ratings)
 df.drop(columns=col_names[0:3])
-user_ids = []
-for i in range(0, N):
-    temp = np.zeros(N, dtype=np.int32)
-    temp[i] = 1
-    user_ids.append(temp)
-X_train, X_test, y_train, y_test = train_test_split(user_ids, ratings_list, test_size=0.3)
+
+# Process user ids
+user_ids = np.zeros([N, N], dtype=np.int32)
+for i in range(0, N):   # foreach user in dataset
+    user_ids[i, i] = 1  # add one-hot encoded entry to input data array
+np.save('user_ids.npy', user_ids)
+
+# Split dataset into train, validation and test sets and save results to file
+X_train, X_test, y_train, y_test = train_test_split(user_ids, user_ratings, test_size=0.3)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.14)
-kfold = StratifiedKFold(n_splits=5)
+np.savez('X_y_data.npz', X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+
+# Split train data into 5 folds and save results to file
+train_ind = []
+test_ind = []
+kfold = KFold(n_splits=5)
+folds = kfold.split(X_train, y_train)
+for j, (train, test) in enumerate(folds):
+    train_ind.append(train)
+    test_ind.append(test)
+np.savez('folds.npz', train_ind=train_ind, test_ind=test_ind)
--- A2_Arch.py
+++ A2_Arch.py
@@ -1,43 +1,83 @@
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Activation
-#
-# model = Sequential([
-#     Dense(32, input_shape=(784,)),
-#     Activation('relu'),
-#     Dense(10),
-#     Activation('softmax'),
-#     ])
-#
-# print("hello!")
-
+import pandas as pd
+import numpy as np
 # import tensorflow as tf
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Dropout, Activation
-# from tensorflow.keras.optimizers import SGD
-#
-# # Generate dummy data
-# import numpy as np
-# x_train = np.random.random((1000, 20))
-# y_train = tf.keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-# x_test = np.random.random((100, 20))
-# y_test = tf.keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
-#
-# model = Sequential()
-# # Dense(64) is a fully-connected layer with 64 hidden units.
-# # in the first layer, you must specify the expected input data shape:
-# # here, 20-dimensional vectors.
-# model.add(Dense(64, activation='relu', input_dim=20))
-# model.add(Dropout(0.5))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(10, activation='softmax'))
-#
-# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-# model.compile(loss='categorical_crossentropy',
-#               optimizer=sgd,
-#               metrics=['accuracy'])
-#
-# model.fit(x_train, y_train,
-#           epochs=20,
-#           batch_size=128)
-# score = model.evaluate(x_test, y_test, batch_size=128)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+
+# Basic variables
+N: int = 943
+M: int = 1682
+
+# Load preprocessed data
+user_ratings = np.load('user_ratings.npy')
+user_ids = np.load('user_ids.npy')
+
+data = np.load('X_y_data.npz', allow_pickle=True)
+X_train = data['X_train']
+y_train = data['y_train']
+X_test = data['X_test']
+y_test = data['y_test']
+data.close()
+
+data = np.load('folds.npz', allow_pickle=True)
+train_ind = data['train_ind']
+test_ind = data['test_ind']
+data.close()
+
+# Train model for different values of H
+print("hello")      # TODO remove
+h = [3, 5, 100]
+exp_metrics = np.zeros([3, 3])
+i = 0
+for H in h:
+    RMSEs = []
+    MAEs = []
+    for j in range(0, 5):
+        # Model
+        model = Sequential()
+        model.add(Dense(N, activation='tanh', input_dim=N))
+        model.add(Dense(H, activation='tanh'))
+        model.add(Dense(M, activation='tanh'))
+
+        def rmse(y_true, y_pred):
+            return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+        model.compile(loss='mean_squared_error', optimizer='sgd', metrics=[rmse, 'mae'])
+
+        # Training
+        history = model.fit(X_train[train_ind[j]], y_train[train_ind[j]], epochs=200, batch_size=50, verbose=0)
+
+        print(j)        # TODO remove
+        # Evaluation
+        loss, rmse, mae = model.evaluate(X_train[test_ind[j]], y_train[test_ind[j]], verbose=0)
+
+        # Store metric values
+        RMSEs.append(rmse)
+        MAEs.append(mae)
+
+        # Plot metrics values at successive epochs
+        if (i == 0) and (j == 0):
+            plt.plot(history.history['rmse'])
+            plt.plot(history.history['mae'])
+            plt.title('Model Metrics')
+            plt.ylabel('Metric')
+            plt.xlabel('Epoch')
+            plt.legend(['RMSE', 'MAE'], loc='upper left')
+            plt.show()
+
+        # Clear session, else may observe impacts of model resets (required by k-fold) on performance
+        K.clear_session()
+    # Store results of cycle in dataframe
+    exp_metrics[i, 0] = H
+    exp_metrics[i, 1] = np.average(RMSEs)
+    exp_metrics[i, 2] = np.average(MAEs)
+    i += 1
+
+# Print results
+table_cols = ['Hidden Layer Neurons (H)', 'RMSE', 'MAE']
+df = pd.DataFrame(data=exp_metrics, columns=table_cols)
+df = df.astype({'Hidden Layer Neurons (H)': np.int32})
+print(df.to_string(formatters=({'Hidden Layer Neurons (H)': '{:d}'.format})))
+# , 'RMSE': '{:,.5f}'.format, 'MAE': '{:,.5f}'.format})))
